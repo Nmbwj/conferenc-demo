81aae1f06df151794d7eac22f931a660bb4a33a0
Nmbwj <naol.mergia1@gmail.com>
2022-10-12 14:42:20 +0300

    Springboot Fundamental trail

---
+++ src/main/java/com/pluralsight/conferencdemo/ConferencDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.conferencdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferencDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferencDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pluralsight/conferencdemo/ConferencDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.conferencdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferencDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
